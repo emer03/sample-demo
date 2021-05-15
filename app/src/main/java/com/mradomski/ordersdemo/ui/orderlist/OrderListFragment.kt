e0745a89db5f03a400b27d4c91f42eec3815430d
m.radomski <emer045@gmail.com>
2021-05-15 17:54:21 +0200

    project initialize

---
+++ app/src/main/java/com/mradomski/ordersdemo/ui/orderlist/OrderListFragment.kt
@@ -0,0 +1,33 @@
+package com.mradomski.ordersdemo.ui.orderlist
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.mradomski.ordersdemo.R
+import com.mradomski.ordersdemo.databinding.OrderListFragmentBinding
+
+class OrderListFragment : Fragment() {
+
+    private lateinit var binding: OrderListFragmentBinding
+    private lateinit var viewModel: OrderListViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.order_list_fragment,
+            container,
+            false
+        )
+
+        viewModel = ViewModelProvider(this).get(OrderListViewModel::class.java)
+
+        return binding.root
+    }
+}
---
+++ app/src/main/java/com/mradomski/ordersdemo/ui/orderlist/OrderListViewModel.kt
@@ -0,0 +1,7 @@
+package com.mradomski.ordersdemo.ui.orderlist
+
+import androidx.lifecycle.ViewModel
+
+class OrderListViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
